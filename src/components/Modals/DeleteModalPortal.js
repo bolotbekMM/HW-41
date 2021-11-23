569747a507b4c9baf34c9ea8aaa854a3039b13ee
bolotbekMM <bolotbek.muratov@gmail.com>
2021-11-23 21:17:00 +0600

    HW---42

--- src/components/Modals/DeleteModalPortal.js
+++ src/components/Modals/DeleteModalPortal.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM  from "react-dom";
-import Backdrop from "../../UI/Backdrop";
+import Backdrop from "../UI/Backdrop";
+import DeleteModal from "./DeleteModal";
 
 const DeleteModalPortal = props => {
     return (
